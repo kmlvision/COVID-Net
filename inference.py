7836cc72fa36a1e8c063a3c6cac4c1b77e2ce457
Philipp Kainz <philipp.kainz@kmlvision.com>
2020-04-11 15:53:53 +0200

    - fixed prediction output to console - updated disclaimer message - fixed output to file when output is specified

--- inference.py
+++ inference.py
@@ -18,14 +18,13 @@ def parse_args():
     parser.add_argument('--metaname', default='model.meta_eval', type=str, help='Name of ckpt meta file')
     parser.add_argument('--ckptname', default='model-2069', type=str, help='Name of model ckpts')
     parser.add_argument('--imagepath', default='assets/ex-covid.jpeg', type=str,
-                        help='Full path to image to be inferenced')
+                        help='Full path to image to be inferred')
     parser.add_argument('--output_csvpath', default='prediction.csv', type=str, help='Path to output csv file')
     parser.add_argument('--output_vispath', default='prediction.png', type=str,
                         help='Path to output visualization file')
     parser.add_argument('--verbose', type=bool, nargs='?', default=False, const=True, help='print verbose output')
 
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 
 def predict(path_image, path_metagraph, path_ckpt, path_output_csv=None, path_output_plot=None, tf_cfg=None):
@@ -63,7 +62,7 @@ def predict(path_image, path_metagraph, path_ckpt, path_output_csv=None, path_ou
         plt.yticks(np.linspace(0, 1, 11))
         for i, v in enumerate(pred):
             plt.text(i - .20, np.clip(v + 0.01, 0, 0.95),
-                     '{:0.2f}'.format(pred[i]),
+                     '{:0.3f}'.format(pred[i]),
                      fontsize=14,
                      color=(0, 0, 0, 0.75))
         try:
@@ -88,24 +87,30 @@ def predict(path_image, path_metagraph, path_ckpt, path_output_csv=None, path_ou
 
     logging.info('Prediction: {}'.format(class_names[pred.argmax()]))
     logging.info('Confidence')
-    logging.info('Normal: {:.3f}, Pneumonia: {:.3f}, COVID-19: {:.3f}'.format(pred[0][0], pred[0][1], pred[0][2]))
+    logging.info('{}: {:.3f}, {}: {:.3f}, {}: {:.3f}'.format(class_names[0], pred[0],
+                                                             class_names[1], pred[1],
+                                                             class_names[2], pred[2]))
 
     # add a disclaimer
     disclaimer_header = '**DISCLAIMER**'
     disclaimer_message = 'Do not use this prediction for self-diagnosis.\n' \
-                         ' You should check with your local authorities for the latest advice on seeking medical' \
+                         'You should check with your local authorities for the latest advice on seeking medical' \
                          ' assistance.\n' \
                          'COVID-Net was developed by Vision and Image Processing Research Group, University of' \
                          ' Waterloo and DarwinAI Corp., Canada.' \
-                         'This software is licensed under the GNU Affero General Public License v3.0. The source code' \
-                         ' and changelog is publicly available from https://github.com/kmlvision/COVID-Net.'
+                         ' This software is licensed under the GNU Affero General Public License v3.0.\n' \
+                         'The source code and changelog is publicly available from' \
+                         ' https://github.com/kmlvision/COVID-Net.'
     logging.info(disclaimer_header)
     logging.info(disclaimer_message)
 
-    if path_output_plot is not None or path_output_csv is not None:
-        with open('disclaimer.txt', 'w') as f:
-            f.write(disclaimer_header + "\n")
-            f.write(disclaimer_message)
+    for p in (path_output_csv, path_output_plot):
+        # write disclaimer files
+        if p is not None:
+            dirname = os.path.abspath(os.path.dirname(p))
+            with open(os.path.join(dirname, 'disclaimer.txt'), 'w') as f:
+                f.write(disclaimer_header + "\n")
+                f.write(disclaimer_message)
 
 
 if __name__ == "__main__":
